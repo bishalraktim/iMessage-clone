005c765728a33a00630e85d59baa253892e532a7
bishalraktim <raktimbishal@gmail.com>
2020-10-11 22:51:07 +1100

    Create sidebar and sidebar chat components

---
+++ src/components/iMessage/Sidebar/Sidebar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import RateReviewIcon from "@material-ui/icons/RateReview";
+import SidebarChat from "./SidebarChat/SidebarChat";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar className="sidebar__avatar" />
+        <div className="sidebar__input">
+          <SearchIcon />
+          <input placeholder="search" />
+        </div>
+        <IconButton variant="outlined" className="slidebar__inputButton">
+          <RateReviewIcon />
+        </IconButton>
+      </div>
+
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
